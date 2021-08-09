35cca40fe0d7b846cf162807358546ff7c8c6227
Brad Chiappetta <brad@greynoise.io>
2021-08-09 11:52:09 -0400

    fix issue

--- misp_modules/modules/expansion/greynoise.py
+++ misp_modules/modules/expansion/greynoise.py
@@ -132,27 +132,26 @@ def handler(q=False):  # noqa: C901
                     vulnerability_object.add_attribute(relation,
                                                        **{'type': attribute_type,
                                                           'value': value})
-            if "malicious" in response["stats"]["classification"]:
-                classifications = response.get(response["stats"]["classification"])
-                for item in classifications:
-                    if item["classification"] == "benign":
-                        value = item["count"]
-                        attribute_type, relation = ('text', 'Benign Scanner Count')
-                        vulnerability_object.add_attribute(relation,
-                                                           **{'type': attribute_type,
-                                                              'value': value})
-                    if item["classification"] == "unknown":
-                        value = item["count"]
-                        attribute_type, relation = ('text', 'Unknown Scanner Count')
-                        vulnerability_object.add_attribute(relation,
-                                                           **{'type': attribute_type,
-                                                              'value': value})
-                    if item["classification"] == "malicious":
-                        value = item["count"]
-                        attribute_type, relation = ('text', 'Malicious Scanner Count')
-                        vulnerability_object.add_attribute(relation,
-                                                           **{'type': attribute_type,
-                                                              'value': value})
+            classifications = response.get(response["stats"]["classification"])
+            for item in classifications:
+                if item["classification"] == "benign":
+                    value = item["count"]
+                    attribute_type, relation = ('text', 'Benign Scanner Count')
+                    vulnerability_object.add_attribute(relation,
+                                                       **{'type': attribute_type,
+                                                          'value': value})
+                if item["classification"] == "unknown":
+                    value = item["count"]
+                    attribute_type, relation = ('text', 'Unknown Scanner Count')
+                    vulnerability_object.add_attribute(relation,
+                                                       **{'type': attribute_type,
+                                                          'value': value})
+                if item["classification"] == "malicious":
+                    value = item["count"]
+                    attribute_type, relation = ('text', 'Malicious Scanner Count')
+                    vulnerability_object.add_attribute(relation,
+                                                       **{'type': attribute_type,
+                                                          'value': value})
             misp_event.add_object(vulnerability_object)
             event = json.loads(misp_event.to_json())
             results = {key: event[key] for key in ('Attribute', 'Object') if
